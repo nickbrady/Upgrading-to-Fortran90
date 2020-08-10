8d862e47f13294da44b030007f53f95e8b3b4078
Nick Brady <nicholas.w.brady@gmail.com>
2020-08-10 13:33:33 +0200

    added sin_in_deg_for_real_scalar and _rank1

--- Chapter_9_Overloading/Fig9_1.f95
+++ Chapter_9_Overloading/Fig9_1.f95
@@ -1,6 +1,6 @@
 MODULE generic_sin_in_deg
     ! Interface blcok associates generic name
-    ! sin_in_deg with four specific names
+    ! sin_in_deg with four specific names.
 
     INTERFACE sin_in_deg
       MODULE PROCEDURE  sin_in_deg_for_real_scalar, &
@@ -10,6 +10,26 @@ MODULE generic_sin_in_deg
 
     END INTERFACE
 
-! TEST SOME MORE
+    INTEGER, PARAMETER :: Dbel = KIND(0D0)
+    REAL, PARAMETER :: Deg_to_rad_real = 0.1745329E-1
+    REAL(Dble), PARAMETER :: Deg_to_rad_dble = 0.17453292519943D-1
+
+CONTAINS
+
+  ! Find sine of scalar of type default REAL.
+
+  FUNCTION sin_in_deg_for_real_scalar (x) RESULT (sine)
+    REAL, INTENT(IN) :: x
+    REAL             :: sine
+    sine = SIN(Deg_to_rad_real * x)
+  END FUNCTION sin_in_deg_for_real_scalar
+
+  ! Find sine of rank 1 array of type default REAL.
+
+  FUNCTION sin_in_deg_for_real_rank1 (x) RESULT (sine)
+    REAL, DIMENSION(:), INTENT(IN) :: x
+    REAL, DIMENSION(SIZE(x))       :: sine
+    sine = SIN (Deg_to_rad_real * x)
+  END FUNCTION sin_in_deg_for_real_rank1
 
 END MODULE generic_sin_in_deg

26cd71e1a3bacf55ea328251198d796afb76a988
Nick Brady <nicholas.w.brady@gmail.com>
2020-08-10 12:25:27 +0200

    Add Interface

--- Chapter_9_Overloading/Fig9_1.f95
+++ Chapter_9_Overloading/Fig9_1.f95
@@ -2,4 +2,12 @@ MODULE generic_sin_in_deg
     ! Interface blcok associates generic name
     ! sin_in_deg with four specific names
 
+    INTERFACE sin_in_deg
+      MODULE PROCEDURE  sin_in_deg_for_real_scalar, &
+                        sin_in_deg_for_real_rank1,  &
+                        sin_in_deg_for_dble_scalar, &
+                        sin_in_deg_for_dble_rank1
+
+    END INTERFACE
+
 END MODULE generic_sin_in_deg

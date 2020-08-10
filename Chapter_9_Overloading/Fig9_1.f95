6d2adbacca6d8dfdbd28fc2397f649e3a9fa84fc
Nick Brady <nicholas.w.brady@gmail.com>
2020-08-10 13:50:52 +0200

    Finished Fig9_1A and 1B

--- Chapter_9_Overloading/Fig9_1.f95
+++ Chapter_9_Overloading/Fig9_1.f95
@@ -10,7 +10,7 @@ MODULE generic_sin_in_deg
 
     END INTERFACE
 
-    INTEGER, PARAMETER :: Dbel = KIND(0D0)
+    INTEGER, PARAMETER :: Dble = KIND(0D0)
     REAL, PARAMETER :: Deg_to_rad_real = 0.1745329E-1
     REAL(Dble), PARAMETER :: Deg_to_rad_dble = 0.17453292519943D-1
 
@@ -49,3 +49,31 @@ CONTAINS
   END FUNCTION sin_in_deg_for_dble_rank1
 
 END MODULE generic_sin_in_deg
+
+
+PROGRAM demo_generic_name
+
+  USE generic_sin_in_deg
+
+  REAL                            :: sine_of_real_scalar
+  REAL, DIMENSION(3)              :: sine_of_real_array
+  REAL(KIND(0D0))                 :: sine_of_dble_scalar
+  REAL(KIND(0D0)), DIMENSION(2)   :: sine_of_dble_array
+
+  ! Invoke sin_in_deg_for_real_scalar.
+  sine_of_real_scalar = sin_in_deg(30.0)
+  PRINT "(E20.12)", sine_of_real_scalar
+
+  !Invoke sin_in_deg_for_real_rank1.
+  sine_of_real_array = sin_in_deg( (/30.0, 60.0, 90.0 /) )
+  PRINT "(3E20.12)", sine_of_real_array
+
+  !Invoke sin_in_deg_for_dble_scalar.
+  sine_of_dble_scalar = sin_in_deg(30D0)
+  PRINT "(E20.12)", sine_of_dble_scalar
+
+  !Invoke sin_in_deg_for_dble_rank1.
+  sine_of_dble_array = sin_in_deg( (/30D0, 60D0 /) )
+  PRINT "(3E20.12)", sine_of_dble_array
+
+END PROGRAM demo_generic_name

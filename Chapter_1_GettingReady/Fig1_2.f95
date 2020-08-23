3cb3ff1b4f9705cc7138c4b61f422846051b1bae
Nick Brady <nicholas.w.brady@gmail.com>
2020-08-23 13:25:40 +0200

    First Chapter 1 commit - Hello World

---
+++ Chapter_1_GettingReady/Fig1_2.f95
@@ -0,0 +1,11 @@
+! Hello World Program
+! The following may be used to confirm the capability of a given
+! human/software/hardware configuration to compile and execute
+! Fortran 90 programs
+
+PROGRAM display_hello_world
+
+  PRINT "(A)", &                ! displays "Hello, World" (without quote marks)
+        " Hello, World"         ! Output is to screen on most computers
+
+END PROGRAM display_hello_world

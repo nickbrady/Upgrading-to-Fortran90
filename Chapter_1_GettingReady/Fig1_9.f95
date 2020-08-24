a1de8c902a7836891e17753ab7b7d34f99af2ae9
Nick Brady <nicholas.w.brady@gmail.com>
2020-08-24 08:52:59 +0200

    Figure 1.9b

--- Chapter_1_GettingReady/Fig1_9.f95
+++ Chapter_1_GettingReady/Fig1_9.f95
@@ -15,7 +15,36 @@ SUBROUTINE find_best_fit_line (x, y, m, b)
   x_avg = SUM(x) / SIZE(x)
   y_avg = SUM(y) / SIZE(y)
 
-  m = SUM ((x - x_avg) * (y - y_avg)) / SUM ((x - x_avg) ** 2)    ! compute slope - m
-  b = y_avg - m * x_avg                                           ! compute intercept - b
+  m = SUM ( (x - x_avg) * (y - y_avg) ) / SUM ( (x - x_avg) ** 2 )    ! compute slope - m
+  b = y_avg - m * x_avg                                               ! compute intercept - b
 
 END SUBROUTINE find_best_fit_line
+
+
+
+PROGRAM test_least_squares
+
+  IMPLICIT NONE
+
+  INTERFACE             ! Interface block containing one interface body follows
+
+    SUBROUTINE find_best_fit_line (x, y, m, b)          ! Interface body for
+      REAL, DIMENSION(:), INTENT (IN)   :: x, y         ! subroutine of Figure 1.9a
+      REAL,               INTENT (OUT)  :: m, b
+    END SUBROUTINE find_best_fit_line
+
+  END INTERFACE
+
+
+  REAL, DIMENSION(4) :: u = (/  5.0, 4.0,  6.0,  8.0 /)
+  REAL, DIMENSION(4) :: v = (/ 14.0, 9.0, 16.0, 23.0 /)
+
+  REAL :: slope, intercept
+
+
+  CALL find_best_fit_line (u, v, slope, intercept)
+
+  PRINT "(A, F6.3)", " slope     = ", slope
+  PRINT "(A, F6.3)", " intercept = ", intercept
+
+END PROGRAM test_least_squares

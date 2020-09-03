134705e3087b8cfaee5ec2c1b4d262142f04ea6d
Nick Brady <nicholas.w.brady@gmail.com>
2020-09-03 11:55:53 +0200

    Figure 8.1

--- Chapter_8_InternalSubprograms/Figure8_01.f95
+++ Chapter_8_InternalSubprograms/Figure8_01.f95
@@ -1,1 +1,29 @@
 SUBROUTINE print_page_header      ! Begin external subroutine
+  ! invoke internal subroutine in each of next 3 statements
+  CALL print_line_centered ("Twelfth Armored Division")
+  CALL print_line_centered ("Daily Flypaper Report")
+  CALL print_line_centered ("May 3, 1992")
+
+CONTAINS                  ! indicates internal subprogram(s) follow
+
+  SUBROUTINE print_line_centered (text)     ! Begin internal subroutine.
+
+    CHARACTER(*), INTENT(IN)  :: text
+
+    INTEGER, PARAMETER  :: Line_length  = 80
+    INTEGER             :: leading_blanks
+
+    leading_blanks = (Line_length - LEN(text) ) / 2
+    PRINT "(A)", REPEAT(" ", leading_blanks) // text
+
+  END SUBROUTINE print_line_centered        ! End internal subroutine.
+
+END SUBROUTINE print_page_header            ! End external subroutine.
+
+
+
+PROGRAM test_print_page_header
+
+  CALL print_page_header      ! Invoke external subroutine.
+
+END PROGRAM test_print_page_header
